18461f54b98c26b5d46e76f92afdfe8fc657fa0d
chad svastisalee <swasgrafx@gmail.com>
2020-09-30 16:53:06 -0500

    README update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # restart_apache
 Bash script for Apache2 Server Restart
+
+## Usage
+Clone the repository or download the latest release. 
+
+From a command-line call re.sh with two arguments.
+1. The vhost configuration
+1. The service directive {restart|reload}
+```sh
+./re.sh 000* restart
+```
--- re.sh
+++ re.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+CONFIG="$1"
+COMMAND="$2"
+
+if [ $# -ne 2 ]
+then
+    echo "$0 requires two parameters {virtual-host} {restart|reload}"
+    exit 1
+fi
+
 # Move the current execution state to the proper directory
 cd /etc/apache2/sites-available
 
 # Disable a vhost configuration
-sudo a2dissite *
-sudo service apache2 restart
+sudo a2dissite "$CONFIG"
+sudo service apache2 "$COMMAND"
 
 # Enable a vhost configuration
-sudo a2ensite *
-sudo service apache2 restart
+sudo a2ensite "$CONFIG"
+sudo service apache2 "$COMMAND"
