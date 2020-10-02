a623e41e6efddd6057bef72fc4983b68e375280d
chad svastisalee <swasgrafx@gmail.com>
2020-10-02 09:31:04 -0500

    Improved messaging

--- re.sh
+++ re.sh
@@ -4,7 +4,7 @@ COMMAND="$2"
 
 if [ $# -ne 2 ]
 then
-    echo "$0 requires two parameters {virtual-host} {restart|reload}"
+    echo "ERROR: $0 requires two parameters {virtual-host} {restart|reload}"
     exit 1
 fi
 
